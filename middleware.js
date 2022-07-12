b0192609771bbc8291c3144a059ac00858d53723
besal101 <bishal.lifesmile@gmail.com>
2022-07-12 13:10:57 +0400

    new

--- middleware.js
+++ middleware.js
@@ -16,5 +16,5 @@ export async function middleware(req, res) {
   url.searchParams.set('city', city);
   url.searchParams.set('region', region);
 
-  return NextResponse.rewrite(JSON.parse(url));
+  return NextResponse.rewrite(url);
 }
