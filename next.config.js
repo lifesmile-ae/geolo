410678d0f4b193fcdfb22f2c51ed93d594835689
besal101 <bishal.lifesmile@gmail.com>
2022-07-12 12:47:02 +0400

    Test New

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
     domains: ['flagcdn.com', 'res.cloudinary.com', 'i.imgur.com'],
   },
   env: {
-    SITEURL: 'https://lifesmile.ae',
+    SITEURL: 'https://testme-seven.vercel.app',
     MONGODB_URI:
       'mongodb+srv://lifesmile:Lm2pibyR888szMG@cluster0.dpayg.mongodb.net/lifesmile?retryWrites=true&w=majority',
     SMTP_HOST: 'smtp.mailtrap.io',
