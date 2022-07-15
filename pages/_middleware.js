fe31c81839d5d30b4cbc491b579ea66d1ebcac1d
besal101 <bishal.lifesmile@gmail.com>
2022-07-15 16:47:52 +0400

    Solved the bug  middelware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,32 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
-import countries from '../utils/countries';
-
-// run only on homepage
-export const config = {
-  matcher: '/',
-};
 
 export async function middleware(req, res) {
   const { nextUrl: url, geo } = req;
-  let mycookie = JSON.parse(req.cookies['country']);
 
-  if (mycookie.country !== geo.country) {
-    const country = geo.country || 'AE';
-    const city = geo.city || 'Dubai';
+  const country = geo.country || 'AE';
+  const city = geo.city || 'Dubai';
 
-    const countryInfo = countries.find((x) => x.cca2 === country);
-    const currencyCode = Object.keys(countryInfo.currencies)[0];
-    const currency = countryInfo.currencies[currencyCode];
-    const languages = Object.values(countryInfo.languages).join(', ');
-    const cookie = JSON.stringify({
-      country,
-      city,
-      currencyCode,
-      currencySymbol: currency.symbol,
-      languages,
-      name: currency.name,
-    });
+  const cookie = JSON.stringify({
+    country,
+    city,
+  });
+
+  if (req.cookies['country']) {
+    let mycookie = JSON.parse(req.cookies['country']);
+    if (mycookie.country !== geo.country) {
+      return NextResponse.next().cookie('country', cookie);
+    } else {
+      return NextResponse.next();
+    }
+  } else {
     return NextResponse.next().cookie('country', cookie);
   }
-  return NextResponse.next();
 }
